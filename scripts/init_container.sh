1f7f243d87891be77d179406cb1d33a082c06987
Jose Luis Bracamonte Amavizca <me@luisjba.com>
2019-05-30 13:14:11 -0700

    Calling the functions into shell_scripts_lib by -c parameter

--- scripts/init_container.sh
+++ scripts/init_container.sh
@@ -94,8 +94,8 @@ chown -R sage:sage /home/sage/.sage/
 sage_install_custom_libraries
 load_extra_sage_libs
 [ $? -eq 0 ] && chown -R sage:sage  ${SAGE_LIBS_DIR}
-su sage -c "/usr/local/bin/shell_scripts_lib.sh ssh_psswordless_configure_localhost /home/sage"
-su sage -c "/usr/local/bin/shell_scripts_lib.sh ssh_permission_status localhost &> /dev/null"
+su sage -c "/usr/local/bin/shell_scripts_lib.sh -c 'ssh_psswordless_configure_localhost' /home/sage"
+su sage -c "/usr/local/bin/shell_scripts_lib.sh -c 'ssh_permission_status localhost' &> /dev/null"
 if [ $? -eq 0 ]; then
     echo "Executing => $@"
     sudo -H -E -u sage $(eval "echo $@")
